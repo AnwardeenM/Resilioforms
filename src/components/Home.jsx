503096fe82c84f5033d1e8b650a1ad32113b17b3
AnwardeenM <kalilanwar597@gmail.com>
2022-11-23 14:22:21 +0530

    working on the validations part

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Button} from "@chakra-ui/react";
 
 const Home = () => {
+    const [userData,setUserData] = useState({})
+    const [isDisabled,setDisabled] = useState(false);
+
+    const handlechange = (e)=>{
+        const {name,value} = e.target;
+        setUserData({...userData,[name]:value}) 
+    }
+    const handleSubmit =()=>{
+        setTimeout(()=>{
+            alert("Registered the data successfully")
+        })
+    }
+
+
   return (
     <div>
         <form action="">
-            <label htmlFor="">Name</label>
-            <input type="text" placeholder='Enter Your Name' />            
-            <label htmlFor="">Age</label>
-            <input type="number" placeholder='Enter Your age' min={18} max={58} />
-            <label htmlFor="">Email</label>
-            <input type="email" placeholder='Enter Your email'/>
-            <label htmlFor="">Date of Birth</label>
-            <input type="date" placeholder='Select the DOB' />
-            <label htmlFor="">Phone</label>
-            <input type="number" minLength={10} maxLength={10}/>
-            {isDisabled ? <Button>Submit</Button> : <div>fill all the details in the form</div>}
+            <label>Name</label>
+            <input type="text" name="name" minLength={4} onChange={handlechange} placeholder='Enter Your Name' />            
+            <label >Age</label>
+            <input type="number" name="age" min={18} max={60} onChange={handlechange} placeholder='Enter Your age' min={18} max={58} />
+            <label >Email</label>
+            <input type="email" name="email" onChange={handlechange} placeholder='Enter Your email'/>
+            <label >Date of Birth</label>
+            <input type="date"  name="dob" onChange={handlechange} placeholder='Select the DOB' />
+            <label >Phone</label>
+            <input type="number" name='mobile' onChange={handlechange} minLength={10} maxLength={10} placeholder="Enter your mobile number"/>
+            {isDisabled ? <Button>Submit</Button> : <div>Please fill all the details in the form</div>}
         </form>
+        <Button>
+            { isShowing? "Show Details" : "Hide Details"}
+        </Button>
     </div>
   )
 }
